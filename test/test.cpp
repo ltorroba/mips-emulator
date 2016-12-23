fb1cf88b917cb2b88ef9bdb63c9b06ff7c366102
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2016-12-23 16:01:44 +0100

    test(test): add get_register test

--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,12 @@
 #define CATCH_CONFIG_MAIN
 #include "../include/catch.hpp"
+
+#include "../src/Emulator.hpp"
+
+TEST_CASE("Registers are retrieved properly", "[get_registers]") {
+    Emulator* vm = new Emulator(128);
+
+    SECTION("Retrieving register 0 will return 0") {
+        REQUIRE(vm->get_register(0) == 0);
+    }
+}
