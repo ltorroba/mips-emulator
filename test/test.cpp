99cf5df5293c1f04a32f65578a8f32ddc8f3b7f8
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2016-12-23 16:33:17 +0100

    test(test): add tests for get & set registers

--- test/test.cpp
+++ test/test.cpp
@@ -3,10 +3,29 @@
 
 #include "../src/Emulator.hpp"
 
-TEST_CASE("Registers are retrieved properly", "[get_registers]") {
+TEST_CASE("Emulator is constructed properly", "[Emulator][get_register]") {
     Emulator* vm = new Emulator(128);
 
-    SECTION("Retrieving register 0 will return 0") {
+    SECTION("Retrieving any regsiter will return 0") {
+        for(int i = 0; i < 32; i++) {
+            REQUIRE(vm->get_register(i) == 0);
+        }
+    }
+}
+
+TEST_CASE("Register get & set are functioning properly", "[get_register][set_register]") {
+    Emulator* vm = new Emulator(128);
+
+    SECTION("Register 0 is always 0") {
         REQUIRE(vm->get_register(0) == 0);
+        vm->set_register(0, 1);
+        REQUIRE(vm->get_register(0) == 0);
+    }
+
+    SECTION("Registers 1-31 can be set to any value") {
+        for(int i = 1; i < 32; i++) {
+            vm->set_register(i, 5);
+            REQUIRE(vm->get_register(i) == 5);
+        }
     }
 }
