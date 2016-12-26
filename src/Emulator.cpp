f2709132861132a4ecd05b2d97fe3acc9358d3c9
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2016-12-26 13:27:46 -0200

    feat(Emulator): implement tgeu operation

--- src/Emulator.cpp
+++ src/Emulator.cpp
@@ -241,6 +241,10 @@ int Emulator::step() {
                     if(Rss >= Rts)
                         return 1;
                     break;
+                case 0b110001: // tgeu
+                    if(Rs >= Rt)
+                        return 1;
+                    break;
             }
             break;
     }
